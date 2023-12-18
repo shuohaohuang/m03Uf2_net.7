3513c87e89be914a067ad39dce687c3bfb8d5569
shuohaohuang <shuohao.huang.7e7@itb.cat>
2023-12-18 22:48:14 +0100

    Refactored and commented

--- ConsoleApp2/Ex19/Program.cs
+++ ConsoleApp2/Ex19/Program.cs
@@ -3,82 +3,83 @@
  * Resume Implementa un programa que demani un nombre romà i retorni el seu valor enter.
  */
 
-using System.Diagnostics.CodeAnalysis;
-using System.Security.Cryptography;
-
-namespace ConsoleApp8
+namespace Roman
 {
-    public class Program
+    public class RomanIntoArabic
     {
         static void Main(string[] args)
         {
-            const string Msg = "Insert a Roman numeral",
-                         ArabicMsg = "The numeral you have written is {0}",
-                         InvalidNum = "Wrong letter was inserted";
+            const string Msg = "Insert a Roman numeral";
 
             string insert;
             Console.WriteLine(Msg);
             insert = Console.ReadLine().ToUpper();
-            if (RomanToArabic(insert) == 0)// roman nums 0 no exists
+            WriteArabicNumber(insert);
+
+        }
+
+        public static void WriteArabicNumber(string romanNumber)
+        {
+            const string ArabicMsg = "The numeral you have written is {0}",
+              InvalidNumMsg = "Wrong roman number was inserted";
+            const int Zero = 0;
+            int arabicNum = RomanToArabic(romanNumber);
+            if (arabicNum == Zero) // in roman nums 0 no exists
             {
-                Console.WriteLine(InvalidNum);
+                Console.WriteLine(InvalidNumMsg);
             }
             else
             {
-                Console.WriteLine(ArabicMsg, RomanToArabic(insert));
+                Console.WriteLine(ArabicMsg, arabicNum);
             }
-
         }
+
         static int RomanToArabic(string romanNumbers)
         {
+            const int WrongValue = 0; // in roman nums 0 no exists
+
             char[] RomanNum = RomanConvertTo(romanNumbers);
             int[] arabicNum = RomanConvertTo(RomanNum);
-            bool correctNum = CorrectRomanNumRule(arabicNum);
-            int result = 0;
+            int result = WrongValue;
 
-            if (correctNum)
+            if (CorrectRomanNumRule(arabicNum))
             {
-                
-                
                 for (int i = 0; i < arabicNum.Length; i++)
                 {
-                    for (int j = i + 1; j == i + 1 && correctNum; j++)
+                    int nextNum = i + 1;
+                    if (nextNum < arabicNum.Length) //avoid ArrayIndexOutOfBoundsException
                     {
-                        if (j < arabicNum.Length)
+                        if (arabicNum[i] >= arabicNum[nextNum])
                         {
-                            if (arabicNum[i] >= arabicNum[j])
-                            {
-
-
-                                result += arabicNum[i];
-                                if (i == arabicNum.Length - 2) result += arabicNum[j];
-                            }
-                            else
-                            {
-                                correctNum = RomanNumOperationRule(arabicNum[i], arabicNum[j]);
-
-                                if (!correctNum) return 0;
-                                result += arabicNum[j] - arabicNum[i];
-                            }
-                        }   
+                            result += arabicNum[i];
+                            if (nextNum == arabicNum.Length - 1) result += arabicNum[nextNum];
+                            //if the next number is the last one, it adds it
+                        }
+                        else
+                        {
+                            if (!RomanNumOperationRule(arabicNum[i], arabicNum[nextNum])) return WrongValue;
+                            result += arabicNum[nextNum] - arabicNum[i];
+                        }
                     }
-                    if (i == 0 && result == 0) result += arabicNum[i];
+                    if (i == WrongValue && result == WrongValue) result += arabicNum[i];
+                    // in case the num only has one digit
                 }
                 return result;
             }
-            return 0; 
+            return WrongValue;
         }
-        static char[] RomanConvertTo(string romanNumbers)
+        static char[] RomanConvertTo(string romanNumbers) //probably useless method 
         {
-            
-            char[] letters = romanNumbers.ToCharArray();
-            return letters; 
+            //returns an array of chars
+            return romanNumbers.ToCharArray();
         }
+
         static int[] RomanConvertTo(char[] letters)
         {
             int[] arbic = new int[letters.Length];
-            int[] wrong = new int[letters.Length];
-            for (int i = 0; i < letters.Length; i++)
+            int[] wrong = new int[letters.Length]; // a void int char
+            for (int i = 0; i < letters.Length; i++) //if the letter is correct, put the corresponding value into the array
+                                                     //else return a void int array
             {
                 switch (letters[i])
                 {
@@ -103,16 +104,18 @@ namespace ConsoleApp8
                     case 'M':
                         arbic[i] = 1000;
                         break;
-                     default :
+                    default:
                         return wrong;
                 }
             }
-            return arbic;
+            return arbic; //if all are correct return a filled int array
         }
 
-        public static bool RomanNumOperationRule(int firstNum, int secondNum)
+        public static bool RomanNumOperationRule(int subtractingNum, int SubtractedNum)
         {
-            return secondNum<=firstNum*10 && secondNum>=firstNum;
+            // I,X,C can only subtract from numbers that are two scales away from them
+            // I can subtract from V and X
+            return SubtractedNum <= subtractingNum * 10 && SubtractedNum >= subtractingNum;
         }
 
         public static bool CorrectRomanNumRule(int[] RomanNums)
@@ -120,33 +123,35 @@ namespace ConsoleApp8
             //Check how many times the same letter appears consecutively; allow a maximum of 3 times, and return false if it occurs four or more times in a row.
             int sameNum = 1;
             int max = 3;
-            for (int i = 0; i < RomanNums.Length; i++)// MIII, length=3, index [0,1,2]. i starts at 0.
+            for (int i = 0; i < RomanNums.Length; i++) // MIII, length=3, index [0,1,2]. i starts at 0.
             {
-                for (int j=i+1; j != RomanNums.Length;j++ )//MIII, lenght=3, index[0,1,2], 
+                for (int j = i + 1; j != RomanNums.Length; j++) //MIII, length=3, index[0,1,2], 
                 {
                     if (RomanNums[i] == RomanNums[j])
                     {
-                        if (!CorrectRomanNumRule(RomanNums, j)) return false;    
+                        if (!CorrectRomanNumRule(RomanNums, j)) return false;
                         sameNum++;
                     }
                     else
                     {
                         sameNum = 1;
                     }
-                    if (!(sameNum<=max) ) return false;
+                    if (!(sameNum <= max)) return false; // if has 4 same int in a row returns false.
                 }
-                sameNum = 1;
+                sameNum = 1; //reset to one for each int in RomanNums
             }
-            return true;
+            return true; //if all are correct returns true;
         }
 
-        public static bool CorrectRomanNumRule(int[]RomanNums, int iterator)
+        public static bool CorrectRomanNumRule(int[] RomanNums, int iterator)
         {
+            //this method checks multiple smaller values before a greater one, like IIX
+
             if (iterator + 1 < RomanNums.Length)
             {
-                return RomanNums[iterator] >= RomanNums[iterator+1];
+                return RomanNums[iterator] >= RomanNums[iterator + 1];
             }
-            return true;
+            return true; //if it is the last digit, it returns true by default.
         }
     }
 }
